b74620e8f067c42ab63a45d20f6faa9f24da649f
Alex Gilbert <118496831+gilbert-alex@users.noreply.github.com>
2024-06-20 15:42:59 -0700

    Answer: part 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,23 @@
-const repeatString = function() {
+const repeatString = function(input, count) {
+
+    /* string method solution
+    return(`${input.repeat(count)}`);
+    */
+
+    // answer with a loop to play along with the instructions
+
+    let output = '';
+
+    if (count < 0) {
+        return 'ERROR';
+    } else {
+        while (count > 0) {
+            output += input;
+            count--;
+        }
+    
+        return output;
+    }
 
 };
 
