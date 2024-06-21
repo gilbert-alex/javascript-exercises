6605f8f5baded5f6d376ab7aff4e85272307d04d
Alex Gilbert <118496831+gilbert-alex@users.noreply.github.com>
2024-06-21 00:18:01 -0700

    Answer: part 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+
+    /*
+    // my solution
+    for (const item of itemsToRemove) {
+        let i = 0;
+        while (i < array.length) {
+            if (array[i] === item) {
+                array.splice(i, 1);
+            } else {
+                i++;
+            }
+        }
+    }
+    return array;
+    */
+
+    /*
+    // TOP solution 1
+    const newArray = [];
+    array.forEach((item) => {
+        if (!itemsToRemove.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
+    */
+
+    // TOP solution 2
+    return array.filter(val => !itemsToRemove.includes(val));
+
 };
 
 // Do not edit below this line
