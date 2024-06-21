db22de7bba0cb98ed8b30d3ae8843aebcc7e7f7a
Alex Gilbert <118496831+gilbert-alex@users.noreply.github.com>
2024-06-21 11:03:19 -0700

    Answer: part 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,28 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    /*
+    // verbose nested if/then stmts as prototype
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+    */
 
+    if (year % 4 === 0 && // every four years
+        // except every 100 years unless every 400 years
+        ( year % 100 !== 0 || year % 400 === 0 )) {
+        return true;
+    } else {
+    return false;
+    }
 };
 
 // Do not edit below this line
