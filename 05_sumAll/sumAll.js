b895ffba4b258161908aff42d77f534eeffdc0ad
Alex Gilbert <118496831+gilbert-alex@users.noreply.github.com>
2024-06-21 00:18:37 -0700

    Answer: part 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
 
+    if (!Number.isInteger(arg1) || !Number.isInteger(arg2)) return 'ERROR';
+    if (arg1 < 0 || arg2 < 0) return 'ERROR';
+    
+    const arr = [arg1, arg2];
+    const arrSorted = arr.sort((a, b) => a - b);
+
+    let counter = arrSorted[0]+1;
+    let max = arrSorted[1]
+    let pos = 1;
+
+    while (counter < max) {
+        arrSorted.splice(pos, 0, counter);
+        counter++;
+        pos++;
+    }
+
+    return arrSorted.reduce((accumulator, currentValue) => accumulator + currentValue);
 };
 
 // Do not edit below this line
