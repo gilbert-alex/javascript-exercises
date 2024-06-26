924d0b1351fd6daf96326a82732883dc4c891ad3
Alex Gilbert <118496831+gilbert-alex@users.noreply.github.com>
2024-06-26 14:32:30 -0700

    Answer: part 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanStr = str.replaceAll(/[\!\s,\.]/g, '').toLowerCase();
+    const forward = Array.from(cleanStr);
+    const backward = Array.from(cleanStr).reverse();
+    return forward.toString() === backward.toString();
 
 };
 
