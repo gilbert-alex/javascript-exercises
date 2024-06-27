8a7b563b70ef35326e15e0d02ad115f4e4dbd443
Alex Gilbert <118496831+gilbert-alex@users.noreply.github.com>
2024-06-26 17:43:34 -0700

    Answer: part 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a, b) => {return (getAge(a) > getAge(b)) ? -1 : 1})
+    return people[0];
+};
 
+const getAge = function(person) {
+    if (person.yearOfDeath === undefined) {
+        let today = new Date(Date.now());
+        let currentYear = today.getFullYear();
+        return currentYear - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
 };
 
 // Do not edit below this line
